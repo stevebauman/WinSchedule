55e0e89a9147a0e93015acc65b76404c160e6932
Steve Bauman <steven_bauman@outlook.com>
2017-11-17 12:41:33 -0500

    Added setting / getting user for logon trigger

--- src/Triggers/Logon.php
+++ src/Triggers/Logon.php
@@ -5,6 +5,32 @@ namespace Stevebauman\WinSchedule\Triggers;
 class Logon extends Trigger
 {
     /**
+     * Sets the logon user of the trigger.
+     *
+     * If null is given, the task is started when any user logs into the computer.
+     *
+     * @param string|null $username
+     *
+     * @return $this
+     */
+    public function setUser($username = null)
+    {
+        $this->object->UserId = $username;
+
+        return $this;
+    }
+
+    /**
+     * Retrieves the logon user of the trigger.
+     *
+     * @return string|null
+     */
+    public function getUser()
+    {
+        return $this->object->UserId;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function type()
